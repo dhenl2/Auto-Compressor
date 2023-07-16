114ecf55a837b4144020359371d72216da89271c
Daniel Henley <69876000+dhenl2@users.noreply.github.com>
2023-07-16 12:40:57 +1000

    Added test files

--- test/mocks/MCP3008.py
+++ test/mocks/MCP3008.py
@@ -5,14 +5,20 @@ class MockMCP3008:
 
     def __init__(self, channel):
         self.channel = channel
-        self.values = []
+        self.values = [1, 2, 3, 4]
         self.value_index = 0
 
     def __getattribute__(self, item):
-        value = self.values[self.value_index]
+        if item == "value":
+            return super(MockMCP3008, self).__getattribute__("mock_get_value")()
+        else:
+            return super(MockMCP3008, self).__getattribute__(item)
+
+    def mock_get_value(self):
+        val = self.values[self.value_index]
         self.value_index += 1
 
-        return value
+        return val
 
     def set_values(self, values):
         self.values = values
--- test/test.py
+++ test/test.py
@@ -1,1 +1,96 @@
+import os
+os.environ["environment"] = "testing"
+
 from src.AutoCompressor import AutoCompressor
+
+
+
+def repeat(value, num):
+    array = []
+    for i in range(num):
+        array.append(value)
+
+    return array
+
+def get_pressure(compressor, target_value):
+    air_sensor = compressor.air_sensor
+
+    return (target_value - air_sensor.c) / air_sensor.m
+
+def get_pressure_samples(compressor, samples, sample_count):
+    all_samples = []
+    for sample in samples:
+        all_samples.extend(repeat(get_pressure(compressor, sample), sample_count))
+
+    return all_samples
+
+
+class MockInflate:
+    def __init__(self):
+        self.inflate_calls = []
+        self.deflate_calls = []
+
+    def mock_inflate(self, t):
+        self.inflate_calls.append(t)
+        return
+
+    def mock_deflate(self, t):
+        self.deflate_calls.append(t)
+        return
+
+
+def test_deflation_times_should_be_positive(mocker):
+    samples = 20
+    mock_compressor = AutoCompressor("./test_config.ini")
+    mock_funcs = MockInflate()
+
+    determine_current_mol_mock = mocker.spy(mock_compressor, "determine_current_mol")
+    mocker.patch.object(mock_compressor, "inflate", mock_funcs.mock_inflate)
+    mocker.patch.object(mock_compressor, "deflate", mock_funcs.mock_deflate)
+    # mock air sensor readings
+    mock_compressor.air_sensor.sensor.values = \
+        get_pressure_samples(mock_compressor, [40, 38, 38, 38, 38, 20, 10], samples)
+    mock_compressor.reach_target(10)
+
+    assert len(mock_funcs.inflate_calls) == 0
+    assert len(mock_funcs.deflate_calls) == 3
+    for call in mock_funcs.deflate_calls:
+        assert 150 > call > 0
+
+def test_inflation_times_should_be_positive(mocker):
+    samples = 20
+    mock_compressor = AutoCompressor("./test_config.ini")
+    mock_funcs = MockInflate()
+
+    determine_current_mol_mock = mocker.spy(mock_compressor, "determine_current_mol")
+    mocker.patch.object(mock_compressor, "inflate", mock_funcs.mock_inflate)
+    mocker.patch.object(mock_compressor, "deflate", mock_funcs.mock_deflate)
+    # mock air sensor readings
+    mock_compressor.air_sensor.sensor.values = \
+        get_pressure_samples(mock_compressor, [10, 12, 12, 12, 12, 28, 38, 40], samples)
+    mock_compressor.reach_target(40)
+
+    assert len(mock_funcs.deflate_calls) == 0
+    assert len(mock_funcs.inflate_calls) == 4
+    for call in mock_funcs.deflate_calls:
+        assert 150 > call > 0
+
+def test_inflation_deflation_times_should_be_positive(mocker):
+    samples = 20
+    mock_compressor = AutoCompressor("./test_config.ini")
+    mock_funcs = MockInflate()
+
+    determine_current_mol_mock = mocker.spy(mock_compressor, "determine_current_mol")
+    mocker.patch.object(mock_compressor, "inflate", mock_funcs.mock_inflate)
+    mocker.patch.object(mock_compressor, "deflate", mock_funcs.mock_deflate)
+    # mock air sensor readings
+    mock_compressor.air_sensor.sensor.values = \
+        get_pressure_samples(mock_compressor, [10, 12, 12, 12, 12, 28, 50, 36, 40], samples)
+    mock_compressor.reach_target(40)
+
+    assert len(mock_funcs.inflate_calls) == 1
+    for call in mock_funcs.inflate_calls:
+        assert 150 > call > 0
+    assert len(mock_funcs.deflate_calls) == 4
+    for call in mock_funcs.deflate_calls:
+        assert 150 > call > 0
